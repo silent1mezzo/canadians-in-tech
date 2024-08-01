f51621cc7ca489b5712ceb5519d2876ba5258d87
Adam McKerlie <adam.mckerlie@replicant.ai>
2024-08-01 13:08:42 -0400

    Update SEO

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,7 +1,7 @@
 export const AppConfig = {
 	site_name: 'Canadians in Tech',
 	title: 'Canadians in Tech',
-	description: 'Celebrating the amazing Canadians in Tech',
+	description: 'A celebration of some of the amazing Canadians in the tech scene! Canadians in Tech aims to celebrate some of the amazing Canadian people and companies in the tech world.',
 	author: 'Adam McKerlie',
 	locale_region: 'en-ca',
 	locale: 'en'
